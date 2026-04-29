27161319515e78d0cd8772efe50414066ef4dc53
Puneet Kumar <puneetshivaay@gmail.com>
2026-04-29 22:20:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "petmets-w801k",
+  "appId": "1:1019671038646:web:7e964b07d10cc9cf4af417",
+  "apiKey": "AIzaSyBJT-NOf5rj4jwlkJTlnZlZga7u5-VB7DE",
+  "authDomain": "petmets-w801k.firebaseapp.com",
+  "measurementId": "G-8XQZBJLL8N",
+  "messagingSenderId": "1019671038646"
+};
