89eda8d355ce3d17cbf22bb3450657c905b9de84
PetMets <info.petmets@gmail.com>
2026-09-10 20:06:52 +0000

    Unhandled Runtime Error
    Error: Element type is invalid: expected a str

--- src/components/layout/main-layout-internal.tsx
+++ src/components/layout/main-layout-internal.tsx
@@ -1,7 +1,7 @@
 "use client"; 
 
 import * as React from "react";
-import Link from "next/navigation";
+import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { AppLogo } from "@/components/icons";
 import { navItems, quickLinks } from "@/config/nav";
